7e880f71fa8e2e0ac44b1cca82f664b05c9da6a9
Madina <djmadi2004@mail.ru>
2018-07-10 15:04:01 +0600

    first commit

---
+++ Producer/PeopleRegister.svc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Producer.pplService;
+using WCFExtensions.Dispatcher;
+namespace Producer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PeopleRegister" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PeopleRegister.svc or PeopleRegister.svc.cs at the Solution Explorer and start debugging.
+
+    [DispatchByBodyElementBehavior]
+    public class PeopleRegister : pplService.person_register
+    {
+        [DispatchBodyElement("personList", "http://persons_register.x-road.ee")]
+
+        public personListResponse personList(personListRequest request)
+        {
+            return new personListResponse
+
+            {
+                client = request.client,
+                service = request.service,
+                id = request.id,
+                protocolVersion =
+          request.protocolVersion,
+                userId = request.userId,
+                //response = new personListResponse
+                //{
+                    person = new person
+                    {
+                        birthDate = DateTime.Now,
+                        firstName = "Juhan",
+                        lastName = "Tamm",
+                        personContact = new contact
+                        {
+                            address = "Metsa 12",
+                            email = new[]{
+                        "juhan@ee.ee"
+                    },
+                            phone = new[]{
+                        "333333"
+                    }
+                        }
+                    }
+                //}
+            };
+        }
+
+    }
+}
