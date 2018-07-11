e3a4734a18b5ae12fc4bab80ed462d71cf33d5d7
Madina <djmadi2004@mail.ru>
2018-07-12 00:28:37 +0600

    Edit project

--- Producer/PeopleRegister.svc.cs
+++ Producer/PeopleRegister.svc.cs
@@ -10,7 +10,7 @@ namespace Producer
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PeopleRegister" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select PeopleRegister.svc or PeopleRegister.svc.cs at the Solution Explorer and start debugging.
-
+    //[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     [DispatchByBodyElementBehavior]
     public class PeopleRegister : pplService.person_register
     {
@@ -18,35 +18,38 @@ namespace Producer
 
         public personListResponse personList(personListRequest request)
         {
-            return new personListResponse
+            if (request.lastName == "Torgoev") { 
+                return new personListResponse
 
-            {
-                client = request.client,
-                service = request.service,
-                id = request.id,
-                protocolVersion =
-          request.protocolVersion,
-                userId = request.userId,
-                //response = new personListResponse
-                //{
-                    person = new person
-                    {
-                        birthDate = DateTime.Now,
-                        firstName = "Juhan",
-                        lastName = "Tamm",
-                        personContact = new contact
+                {
+                    client = request.client,
+                    service = request.service,
+                    id = request.id,
+                    protocolVersion = request.protocolVersion,
+                    userId = request.userId,
+
+                        person = new person
                         {
-                            address = "Metsa 12",
-                            email = new[]{
-                        "juhan@ee.ee"
-                    },
-                            phone = new[]{
-                        "333333"
-                    }
+                            birthDate = DateTime.Now,
+                            firstName = "Ilgiz",
+                            lastName = "Torgoev",
+                            personContact = new contact
+                            {
+                                address = "Chuy 12",
+                                email = new[]{
+                                    "juhan@ee.ee","juhan2@ee.ee","juha3n@ee.ee"
+                                },
+                                phone = new[]{
+                                    "0701444444","0701222222","0701333333"
+                                }
+                            }
                         }
-                    }
-                //}
-            };
+                };
+            }
+            else
+            {
+                return new personListResponse();
+            }
         }
 
     }
